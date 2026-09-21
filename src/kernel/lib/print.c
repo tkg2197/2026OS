f6f9cf44eb512bfeba20e59bc40406ed1c29c81a
tkg <2197322347@qq.com>
2026-09-21 21:19:51 +0800

    lab1完成

---
+++ src/kernel/lib/print.c
@@ -0,0 +1,145 @@
+/* 标准输出和报错机制 */
+
+#include <stdarg.h>
+#include "mod.h"
+
+static char digits[] = "0123456789abcdef";
+
+/* printf的自旋锁 */
+static spinlock_t print_lk;
+
+/* 初始化uart + 初始化printf锁 */
+void print_init(void)
+{
+    uart_init();
+    spinlock_init(&print_lk, "printf");
+}
+
+/* %d %p */
+static void printint(int xx, int base, int sign)
+{
+    char buf[16];
+    int i;
+    uint32 x;
+
+    if (sign && (sign = xx < 0))
+        x = -xx;
+    else
+        x = xx;
+
+    i = 0;
+    do
+    {
+        buf[i++] = digits[x % base];
+    } while ((x /= base) != 0);
+
+    if (sign)
+        buf[i++] = '-';
+
+    while (--i >= 0)
+        uart_putc_sync(buf[i]);
+}
+
+/* %x */
+static void printptr(uint64 x)
+{
+    uart_putc_sync('0');
+    uart_putc_sync('x');
+    for (int i = 0; i < (sizeof(uint64) * 2); i++, x <<= 4)
+        uart_putc_sync(digits[x >> (sizeof(uint64) * 8 - 4)]);
+}
+
+/*
+    标准化输出, 需要支持:
+    1. %d (32位有符号数,以10进制输出)
+    2. %p (32位无符号数,以16进制输出)
+    3. %x (64位无符号数,以0x开头的16进制输出)
+    4. %c (单个字符)
+    5. %s (字符串)
+    提示: stdarg.h中的va_list中包括你需要的参数地址
+*/
+void printf(const char *fmt, ...)
+{
+    va_list ap;
+    va_start(ap, fmt);
+    spinlock_acquire(&print_lk);
+    for(int i = 0;fmt[i] != '\0';i++) {
+        if(fmt[i] != '%') {
+            uart_putc_sync(fmt[i]);
+            continue;
+        }
+        i++;
+        if(fmt[i] == '\0') {
+            uart_putc_sync('%');
+            break;
+        }
+        switch (fmt[i]) {
+            case 'd': {
+                int value = va_arg(ap, int);
+                printint(value, 10, 1);
+                break;
+            } 
+
+            case 'p': {
+                uint32 value = va_arg(ap, uint32);
+                printint(value, 16, 0);
+                break;
+            } 
+
+            case 'x': {
+                uint64 value = va_arg(ap, uint64);
+                printptr(value);
+                break;
+            } 
+
+            case 'c': {
+                int value = va_arg(ap, int);
+                uart_putc_sync(value);
+                break;
+            } 
+
+            case 's': {
+                char * str = va_arg(ap, char *);
+                if (str == NULL) {
+                    str = "(null)";
+                }
+                while (*str != '\0') {
+                    uart_putc_sync(*str);
+                    str++;
+                }
+                break;
+            } 
+            case '%': 
+                uart_putc_sync('%');
+                break;
+            default:
+                uart_putc_sync('%');
+                uart_putc_sync(fmt[i]);
+                break;
+        }
+    }
+    spinlock_release(&print_lk);
+    va_end(ap);
+}
+
+
+
+/* 如果发生panic, UART的停止标志 */
+volatile int panicked = 0;
+
+/* 报错并终止输出 */
+void panic(const char *s)
+{
+    printf("panic! %s\n", s);
+    panicked = 1;
+    while (1)
+        ;
+}
+
+/* 如果不满足条件, 则调用panic */
+void assert(bool condition, const char *warning)
+{
+    if (!condition) {
+        panic(warning);
+    }
+}
---
+++ src/kernel/main.c
@@ -0,0 +1,21 @@
+#include "arch/mod.h"
+#include "lib/mod.h"
+
+volatile static int started = 0;
+
+int main()
+{
+    int cpuid = r_tp();    
+    if (cpuid == 0) {
+        print_init();
+        printf("cpu %d is booting!\n", cpuid);
+        __sync_synchronize();
+        started = 1;
+    }
+    else {
+        while(started == 0);
+        __sync_synchronize();
+        printf("cpu %d is booting!\n", cpuid);
+    }
+    while(1);
+}
